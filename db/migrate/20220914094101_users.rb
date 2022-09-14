93a7f5fc2a005625d9e7bb1b8f381589fe7af409
ilham bouaik <ibouaik@gmail.com>
2022-09-14 11:20:59 +0100

    create tables

--- db/migrate/20220914094101_users.rb
+++ db/migrate/20220914094101_users.rb
@@ -1,6 +1,6 @@
 class Users < ActiveRecord::Migration[7.0]
   def change
-     create_table :users do |t|
+  create_table "users", force: :cascade do |t|
       t.string :name
       t.timestamps
     end
---
+++ db/migrate/20220914101840_recipe_foods.rb
@@ -0,0 +1,9 @@
+class RecipeFoods < ActiveRecord::Migration[7.0]
+  def change
+      create_table "recioe_foods", force: :cascade do |t|
+        t.integer :quantity
+        t.integer :recipe_id
+        t.integer :food_id
+      end
+  end
+end
