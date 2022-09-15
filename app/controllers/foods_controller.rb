81882dd1a73f537fbde7eb39f4fb2712527fee9f
ilham bouaik <ibouaik@gmail.com>
2022-09-15 18:44:28 +0100

    add validates to the food model and create method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,4 +7,26 @@ class FoodsController < ApplicationController
   def new
     @food = Food.new
   end
+
+def create
+  @food = Food.new(food_params)
+
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to artical_url(@food), notice: "Food was successfully created." }
+        format.json { render :show, status: :created, location: @food }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
+end
+
+
+
+private
+
+def food_params
+  params.require(:food).permit(:name, :measurement_unit, :price)
+end
 end
