d96aa70f80daeb1fc32fdd26883f978448398c8e
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-09-16 02:58:57 +0300

    Fix linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,35 +1,34 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = current_user.recipes
+  end
 
-    def index
-        @recipes = current_user.recipes
-    end   
+  def new
+    @recipe = Recipe.new
+  end
 
-    def new
-        @recipe = Recipe.new
-    end
-
-    def create
-        @recipe = current_user.recipes.build(recipe_params)
-        respond_to do |format|
-          format.html do
-            if @recipe.save
-              redirect_to recipes_path, notice: 'Recipe created successfully'
-            else
-              redirect_to new_recipe_path, alert: 'Error, Recipe not created!'
-            end
-          end
-        end
-    end
-
-    def destroy
-        respond_to do |format|
-          format.html do
-            if @recipe.destroy
-              redirect_to recipes_path, notice: 'Recipe deleted successfully'
-            else
-              redirect_to recipes_path, alert: 'Error, Recipe not deleted!'
-            end
-          end
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    respond_to do |format|
+      format.html do
+        if @recipe.save
+          redirect_to recipes_path, notice: 'Recipe created successfully'
+        else
+          redirect_to new_recipe_path, alert: 'Error, Recipe not created!'
         end
       end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      format.html do
+        if @recipe.destroy
+          redirect_to recipes_path, notice: 'Recipe deleted successfully'
+        else
+          redirect_to recipes_path, alert: 'Error, Recipe not deleted!'
+        end
+      end
+    end
+  end
 end
