4f41c6ac838611ca70547b5b19c161daa35684a9
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-09-16 01:35:38 +0300

    Add create action to recipes contraller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,17 @@ class RecipesController < ApplicationController
     def new
         @recipe = Recipe.new
     end
+
+    def create
+        @recipe = current_user.recipes.build(recipe_params)
+        respond_to do |format|
+          format.html do
+            if @recipe.save
+              redirect_to recipes_path, notice: 'Recipe created successfully'
+            else
+              redirect_to new_recipe_path, alert: 'Error, Recipe not created!'
+            end
+          end
+        end
+      end
 end
