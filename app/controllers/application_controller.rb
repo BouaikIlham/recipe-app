ab568bff09795044368070386d934382a078a3c0
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-09-17 12:24:51 +0300

    Add navigation bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+
+
 class ApplicationController < ActionController::Base
-end
+    before_action :authenticate_user!
+    protect_from_forgery prepend: true
+    before_action :update_allowed_parameters, if: :devise_controller?
+  
+    protected
+  
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    end
+  end
