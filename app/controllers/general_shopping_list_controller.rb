cb78914afcdeee48e39dbde45ba276cbb75056c1
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-09-17 03:19:04 +0300

    Fix linter errors

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,5 +1,5 @@
 class GeneralShoppingListController < ApplicationController
-    def index
-        @recipes = current_user.recipes
-    end
+  def index
+    @recipes = current_user.recipes
   end
+end
